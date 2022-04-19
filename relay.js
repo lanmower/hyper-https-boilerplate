4b09f318687683ae6e062e001508895e1a2a6b3c
lanmower <almagestfraternite@gmail.com>
2022-04-19 16:17:30 +0200

    Create relay.js

---
+++ relay.js
@@ -0,0 +1,30 @@
+const DHT = require("@hyperswarm/dht");
+const crypto = require("hypercore-crypto");
+var net = require("net");
+var pump = require("pump");
+const node = new DHT({});
+
+module.exports = () => {
+  return {
+    serve: (key, port, addr) => {
+      const keyPair = crypto.keyPair(crypto.data(Buffer.from(key)));
+      const server = node.createServer();
+      server.on("connection", function(servsock) {
+        servsock.once("data", function(data) {
+          if(data == 'http') {
+            console.log('http');
+            socket = net.connect(80, addr);
+          }
+          if(data == 'https') {
+            console.log('https');
+            socket = net.connect(443, addr);
+          }
+          pump(servsock, socket, servsock);
+        });
+
+      });
+      server.listen(keyPair);
+      return keyPair.publicKey;
+    }
+  };
+};
