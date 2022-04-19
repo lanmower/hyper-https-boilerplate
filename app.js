46149cb49c88be0379f79850100edb34a1c1224f
lanmower <almagestfraternite@gmail.com>
2022-04-19 21:21:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+})*/
+
+var proxy = require('express-http-proxy');
+app.use('/', proxy('www.google.com'));
+
 
 module.exports = app;
+
+
