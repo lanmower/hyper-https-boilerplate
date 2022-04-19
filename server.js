61736d99312667f8a52452bc0aaa702377434a9b
lanmower <almagestfraternite@gmail.com>
2022-04-19 16:17:15 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+"use strict";
+var b32 = require("hi-base32");
+console.log(process.argv.length)
+if(process.argv.length == 3) {
+  const key = process.argv[process.argv.length-1];
+  const out = require('./relay.js')().serve(key, 80, "127.0.0.1");
+  console.log('listening', b32.encode(out).replace('====','').toLowerCase());
+  console.log('hex', out);
+} else console.log("usage: hyper-tcp-relay-server <port> <addr> <key>");
+
+var app = require("./app.js");
+ 
+require("greenlock-express")
+    .init({
+        packageRoot: __dirname,
+        configDir: "./greenlock.d",
+        maintainerEmail: "jon@example.com",
+        cluster: false
+    })
+    .serve(app);
