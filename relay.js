bf84ccef89ff72002fcb06aa7b9d3af0d89aa19c
lanmower <almagestfraternite@gmail.com>
2022-04-20 23:50:51 +0200

    Update relay.js

--- relay.js
+++ relay.js
@@ -6,16 +6,17 @@ const node = new DHT({});
 
 module.exports = () => {
   return {
-    serve: (key, port, addr) => {
+    serve: (key, port, secureport, addr) => {
       const keyPair = crypto.keyPair(crypto.data(Buffer.from(key)));
       const server = node.createServer();
       server.on("connection", function(servsock) {
+        console.log('connection');
         servsock.once("data", function(data) {
           if(data == 'http') {
-            socket = net.connect(80, addr);
+            socket = net.connect(port, addr);
           }
           if(data == 'https') {
-            socket = net.connect(443, addr);
+            socket = net.connect(secureport, addr);
           }
           pump(servsock, socket, servsock);
         });
