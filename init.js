6f3a0f5726a3974d1f79758e2a09842783185be0
lanmower <almagestfraternite@gmail.com>
2022-04-26 21:27:09 +0200

    Update init.js

--- init.js
+++ init.js
@@ -2,3 +2,6 @@ const fs = require('fs');
 const b32 = require('hi-base32');
 require('dotenv').config();
 
+const crypto = require("hypercore-crypto");
+const keyPair = crypto.keyPair(crypto.data(Buffer.from(key)));
+const out = require('./relay.js')().serve(process.env.KEY, 2080, 2443, "127.0.0.1");
