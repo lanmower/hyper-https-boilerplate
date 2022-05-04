7b8e142056e86c25db8564dfec1147f8c5ea431a
lanmower <almagestfraternite@gmail.com>
2022-05-04 18:55:12 +0200

    Update init.js

--- init.js
+++ init.js
@@ -1,7 +1,28 @@
 const fs = require('fs');
 const b32 = require('hi-base32');
-require('dotenv').config();
-
 const crypto = require("hypercore-crypto");
-const keyPair = crypto.keyPair(crypto.data(Buffer.from(key)));
-const out = require('./relay.js')().serve(process.env.KEY, 2080, 2443, "127.0.0.1");
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter a unique identifier: ', function (password) {
+  rl.question('Enter your contact email: ', function (email) {
+    const keyPair = crypto.keyPair(crypto.data(Buffer.from(password)));
+    const bkey = b32.encode(keyPair.publicKey).replace('====','').toLowerCase();
+    console.log('Address will be: ', bkey+".matic.ml");
+    fs.mkdirSync('tunnel/greenlock.d/', { recursive: true }, (err) => {console.log(err)});
+    fs.writeFileSync('.env', 'KEY='+password);
+    fs.writeFileSync('greenlock.d/config.json', JSON.stringify({sites: [{subject:bkey+".matic.ml"}],defaults: {subscriberEmail: email}}));
+    console.log('Password will be: '+keyPair.secretKey.toString('base64').replace('==',''));
+    fs.writeFileSync('address', bkey+".matic.ml");
+    rl.close();
+  })
+});
+
+rl.on('close', function () {
+  console.log('\nBYE BYE !!!');
+  process.exit(0);
+});
